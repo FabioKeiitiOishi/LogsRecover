5413441ef937769ac25cca3556d7c9c40cc34741
FabioKeiitiOishi <fabio.oishi@outlook.com>
2021-02-05 01:32:25 -0300

    Changing frontend

--- LogRecovery/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ LogRecovery/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -6,18 +6,18 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public logs: LogVM[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<LogVM[]>(baseUrl + 'api/log').subscribe(result => {
+      this.logs = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface LogVM {
+  id: number;
+  ip: string;
+  recordedTime: Date;
+  userAgent: string;
 }
